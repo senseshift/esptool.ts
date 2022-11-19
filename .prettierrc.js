f8c041911878d50b591a6cedff275d17d428df5a
leon0399 <hello@leon0399.ru>
2022-11-19 20:50:32 +0300

    chore: update build pipeline and linters

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,7 +2,8 @@ module.exports = {
   trailingComma: "es5",
   tabWidth: 2,
   semi: true,
-  singleQuote: false,
+  singleQuote: true,
   printWidth: 120,
-  arrowParens: "always"
+  arrowParens: "always",
+  endOfLine: "lf",
 };
