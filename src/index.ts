70f16de677feb00d2f3c6f2d49ddb746acdb5021
Leonid Meleshin <hello@leon0399.ru>
2023-10-09 12:59:09 +0400

    feat: add license header linter

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 // Copyright (C) 2021 Toitware ApS. All rights reserved.
+// Copyright (C) 2023 Leonid Meleshin. All rights reserved.
 // Use of this source code is governed by an MIT-style license that can be
 // found in the LICENSE file.
 
